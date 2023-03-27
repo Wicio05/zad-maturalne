facce75e9a5979d13bae58eee91259c0b5d35c76
Wicio <witekcybul1@gmail.com>
2023-03-27 21:13:07 +0200

    fixing bugs

--- include/helper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-template <class T>
-void getData(std::vector<T> &vec);
-
-int countFactors(int num, bool countDiff);
--- include/questhandler.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void zad4_1(int &size, int &num);
-
-void zad4_2(int &num, int &diffNum, int &factorCount, int &diffFactorCount);
