31ad2a1621805705c639f0e6ea51e9f0ca659b9e
Wicio <witekcybul1@gmail.com>
2023-03-27 18:06:54 +0200

    improving file handling

--- main.cpp
+++ main.cpp
@@ -4,24 +4,43 @@
 #include <string>
 #include <vector>
 
+template <class T>
+void getData(std::vector<T> &vec);
+
 int countFactors(int num, bool countDiff);
 
-void zad4_1(std::vector<std::string> &nums);
+void zad4_1(int &size, int &num);
 
 void zad4_2(int &num, int &diffNum, int &factorCount, int &diffFactorCount);
 
 int main()
 {
-    std::vector<std::string> nums;
-    int num, diffNum, factorCount, diffFactorCount;
+    int digits, factorNum, secondNum, diffNum, factorCount, diffFactorCount, size;
 
-    zad4_1(nums);
-    zad4_2(num, diffNum, factorCount, diffFactorCount);
+    zad4_1(size, digits);
+    zad4_2(factorNum, diffNum, factorCount, diffFactorCount);
 
     std::ofstream f_result("wynik4.txt");
-    f_result << "4.1 " << nums.size() - 1 << " " << nums[0] << "\n";
-    f_result << "4.2 " << num << " " << factorCount << " " << diffNum << " " << diffFactorCount << "\n";
+    f_result << "4.1 " << size - 1 << " " << digits << "\n";
+    f_result << "4.2 " << factorNum << " " << factorCount << " " << diffNum << " " << diffFactorCount << "\n";
     f_result.close();
+}
+
+template <class T>
+void getData(std::vector<T> &vec)
+{
+    std::ifstream f_data("liczby.txt");
+
+    if(!f_data)
+        std::cout << "Couldn't open liczby.txt\n";
+    
+    while (f_data.good())
+    {
+        T temp;
+        f_data >> temp;
+        vec.push_back(temp);
+    }
+    f_data.close();
 }
 
 int countFactors(int num, bool countDiff)
@@ -41,39 +60,29 @@ int countFactors(int num, bool countDiff)
     return factorCount + 1;
 }
 
-void zad4_1(std::vector<std::string> &nums)
+void zad4_1(int &size, int &num)
 {
-    std::ifstream f_data("liczby.txt");
+    size = num = 0;
+    std::vector<std::string> nums;
+    getData(nums);
 
-    if(!f_data)
-        std::cout << "Couldn't open liczby.txt\n";
-    
-    while (f_data.good())
+    for(int i{0}; i < nums.size(); ++i)
     {
-        std::string temp;
-        f_data >> temp;
-        if (temp[0] == temp[temp.length() - 1])
-            nums.push_back(temp);
+        if(nums[i][0] == nums[i][nums[i].length() - 1])
+        {
+            if(num == 0)
+                num = std::stoi(nums[i]);
+            
+            size++;
+        }
     }
-    f_data.close();
 }
 
 void zad4_2(int &num, int &diffNum, int &factorCount, int &diffFactorCount)
 {
     factorCount = diffFactorCount = 0;
     std::vector<int> nums;
-    std::ifstream f_data("liczby.txt");
-
-    if(!f_data)
-        std::cout << "Couldn't open liczby.txt\n";
-   
-    while (f_data.good())
-    {
-        int temp;
-        f_data >> temp;
-        nums.push_back(temp);
-    }
-    f_data.close();
+    getData(nums);
 
     for(int i{0}; i < nums.size(); ++i)
     {
